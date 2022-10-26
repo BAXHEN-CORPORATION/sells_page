0cc77c8a358c37b31e4b87e7c1ec7e512b9238b1
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-10-26 00:54:03 -0300

    adjust rules

--- src/schema/location/location.resolver.ts
+++ src/schema/location/location.resolver.ts
@@ -7,31 +7,51 @@ import { IPGeolocationResponse, IpResponse, WorldTimeResponse } from "./types";
 export class LocationResolver {
   @Query(() => Location)
   async location(): Promise<Location> {
-    const ipResponse = await axios.get<IpResponse>("http://ip-api.com/json");
+    try {
+      const ipResponse = await axios.get<IpResponse>("http://ip-api.com/json");
 
-    const ip = ipResponse.data.query;
+      const ip = ipResponse.data.query;
 
-    const worldTimeResponse = await axios.get<WorldTimeResponse>(
-      `http://worldtimeapi.org/api/ip/${ip}`
-    );
+      const worldTimeResponse = await axios.get<WorldTimeResponse>(
+        `http://worldtimeapi.org/api/ip/${ip}`
+      );
 
-    const ipGeolocationResponse = await axios
-      .get<IPGeolocationResponse>(
-        `https://api.ipbase.com/v2/info?apikey=${process.env.API_KEY_IP_LOCATION}&ip=${ip}`
-      )
-      .catch(() => {});
+      const ipGeolocationResponse = await axios
+        .get<IPGeolocationResponse>(
+          `https://api.ipbase.com/v2/info?apikey=${process.env.API_KEY_IP_LOCATION}&ip=${ip}`
+        )
+        .catch(() => {});
 
-    const city = ipGeolocationResponse?.data?.data?.location?.city || {
-      name: "",
-      name_translated: "",
-    };
-    const country = ipGeolocationResponse?.data?.data?.location?.country || {
-      name: "",
-      name_translated: "",
-    };
+      const city = ipGeolocationResponse?.data?.data?.location?.city || {
+        name: "",
+        name_translated: "",
+      };
+      const country = ipGeolocationResponse?.data?.data?.location?.country || {
+        name: "",
+        name_translated: "",
+      };
 
-    const result = { ...worldTimeResponse.data, city, country };
+      const result = { ...worldTimeResponse.data, city, country };
 
-    return result;
+      return result;
+    } catch (error) {
+      return {
+        datetime: "",
+        day_of_week: 0,
+        day_of_year: 0,
+        dst: false,
+        city: { name: "", name_translated: "" },
+        country: { name: "", name_translated: "" },
+        dst_offset: 0,
+        utc_datetime: "",
+        client_ip: "",
+        abbreviation: "",
+        unixtime: 0,
+        utc_offset: "",
+        week_number: 0,
+        raw_offset: 0,
+        timezone: "",
+      };
+    }
   }
 }
