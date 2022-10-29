44f4d85f79f9ca2eda68fba9bc43d7b0b3031d79
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-10-29 00:15:17 -0300

    chore: update server code

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,5 +1,4 @@
-import { ApolloServer } from "apollo-server-micro";
-import { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer } from "apollo-server-lambda";
 import "reflect-metadata";
 
 import { LocationResolver } from "schema/location";
@@ -7,25 +6,25 @@ import { QuoteResolver } from "schema/quotes";
 
 import { buildSchema } from "type-graphql";
 
-const schema = await buildSchema({
-  resolvers: [LocationResolver, QuoteResolver],
-});
-
-const server = new ApolloServer({
-  schema,
-
-  introspection: true,
-  context(ctx) {
-    return ctx;
-  },
-});
-
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-const handler = server.createHandler({ path: "/api/graphql" });
+const handler = async function (event: any, context: any) {
+  const schema = await buildSchema({
+    resolvers: [LocationResolver, QuoteResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  return new Promise((yay, nay) => {
+    const cb = (err: any, args: any) => (err ? nay(err) : yay(args));
+    server.createHandler()(event, context, cb);
+  });
+};
 
 export default handler;
