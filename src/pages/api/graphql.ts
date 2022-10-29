a4b47c543f52fd70e8f3110411432315358cf1fe
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-10-29 01:47:58 -0300

    chore: update server code

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,34 +1,76 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-express";
 import { NextApiRequest, NextApiResponse } from "next";
+import { buildSchema } from "type-graphql";
+import { processRequest } from "graphql-upload";
 import "reflect-metadata";
 
 import { LocationResolver } from "schema/location";
 import { QuoteResolver } from "schema/quotes";
 
-import { buildSchema } from "type-graphql";
-
 const schema = await buildSchema({
   resolvers: [LocationResolver, QuoteResolver],
 });
 
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  fn: any
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 const server = new ApolloServer({
   schema,
-  cache: "bounded",
-  persistedQueries: false,
+  context: (ctx) => ctx,
 });
+
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const contentType = req.headers["content-type"];
+
+  if (contentType && contentType.startsWith("multipart/form-data")) {
+    req.body = await processRequest(req, res);
+  }
+  const apolloMiddleware = server.getMiddleware({
+    path: "/api/graphql",
+  });
+
+  return runMiddleware(req, res, apolloMiddleware);
+}
+export default handler;
 
 export const config = {
   api: {
     bodyParser: false,
+    externalResolver: true,
   },
 };
 
-const startServer = server.start();
+// const server = new ApolloServer({
+//   schema,
+//   cache: "bounded",
+//   persistedQueries: false,
+// });
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  await startServer;
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-  await server.createHandler({ path: "/api/graphql" })(req, res);
-};
+// const startServer = server.start();
 
-export default handler;
+// const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+//   await startServer;
+
+//   await server.createHandler({ path: "/api/graphql" })(req, res);
+// };
+
+// export default handler;
