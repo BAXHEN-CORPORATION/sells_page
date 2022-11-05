b772912e610ad28786524f31de306ce44d0ea49b
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-11-05 16:28:49 -0300

    SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from "next";
 import React from "react";
+import Head from "next/head";
+import type { NextPage } from "next";
 
 import { Box, Stack } from "@mui/material";
 import { dehydrate, useQuery } from "react-query";
@@ -21,6 +22,8 @@ import { getDate, getHoursFormatted, getIsDay, getTimezone } from "utils/date";
 import { desktopImages, mobileImages, tabletImages } from "utils/images";
 import { getLocationInfo } from "utils/location";
 import Backlinks from "components/Backlinks";
+
+import ogg_image from "assets/images/SEO/ogg_image.png";
 
 export type TTime = "day" | "night";
 
@@ -67,63 +70,82 @@ const Home: NextPage = () => {
   }, [isDay]);
 
   return (
-    <Box
-      minHeight="100vh"
-      width="100%"
-      sx={{
-        position: "relative",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundImage: ({ functions: { linearGradientImage } }) => ({
-          mobile: linearGradientImage({
-            primary: "rgba(0, 0, 0, 0.4)",
-            url: mobileImages[time].src,
-          }),
-          tablet: linearGradientImage({
-            primary: "rgba(0, 0, 0, 0.4)",
-            url: tabletImages[time].src,
-          }),
-          desktop: linearGradientImage({
-            primary: "rgba(0, 0, 0, 0.4)",
-            url: desktopImages[time].src,
-          }),
-        }),
-      }}
-    >
-      <Stack
-        minHeight="inherit"
-        padding={{ mobile: "32px 25px", tablet: "80px 64px" }}
-        paddingBottom="40px"
-      >
-        <Quote
-          onClick={onQuoteRefresh}
-          show={showMore}
-          author={author}
-          quote={quote}
+    <>
+      <Head>
+        <title>Clock App Challenge - MUI 5| NextJs | GraphQL</title>
+        <meta
+          name="description"
+          content="Clock App design made with MUI 5, NextJs and GraphQL. This a challenge made by Leonardo Rebouças."
+          key="desc"
         />
-
+        <meta property="og:title" content="Clock App" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://clock-app.baxhen.com/" />
+        <meta property="og:image" content={ogg_image.src} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <Box
+        minHeight="100vh"
+        width="100%"
+        sx={{
+          position: "relative",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundImage: ({ functions: { linearGradientImage } }) => ({
+            mobile: linearGradientImage({
+              primary: "rgba(0, 0, 0, 0.4)",
+              url: mobileImages[time].src,
+            }),
+            tablet: linearGradientImage({
+              primary: "rgba(0, 0, 0, 0.4)",
+              url: tabletImages[time].src,
+            }),
+            desktop: linearGradientImage({
+              primary: "rgba(0, 0, 0, 0.4)",
+              url: desktopImages[time].src,
+            }),
+          }),
+        }}
+      >
         <Stack
-          sx={{ marginTop: "auto !important", marginBottom: "3rem !important" }}
+          minHeight="inherit"
+          padding={{ mobile: "32px 25px", tablet: "80px 64px" }}
+          paddingBottom="40px"
         >
-          <Greeting time={time} />
-          <Time hours={hours} timezone={timezone} />
-          <Location
-            city={city}
-            country={country}
+          <Quote
+            onClick={onQuoteRefresh}
             show={showMore}
-            onShowClick={(_, show) => setShowMore(show)}
+            author={author}
+            quote={quote}
           />
-        </Stack>
 
-        <TimezoneDetails show={showMore} time={time} info={timezoneInfo} />
-      </Stack>
-      <Backlinks
-        links={[
-          "https://leonardo.baxhen.com",
-          "https://product-preview.baxhen.com",
-        ]}
-      />
-    </Box>
+          <Stack
+            sx={{
+              marginTop: "auto !important",
+              marginBottom: "3rem !important",
+            }}
+          >
+            <Greeting time={time} />
+            <Time hours={hours} timezone={timezone} />
+            <Location
+              city={city}
+              country={country}
+              show={showMore}
+              onShowClick={(_, show) => setShowMore(show)}
+            />
+          </Stack>
+
+          <TimezoneDetails show={showMore} time={time} info={timezoneInfo} />
+        </Stack>
+        <Backlinks
+          links={[
+            "https://leonardo.baxhen.com",
+            "https://product-preview.baxhen.com",
+          ]}
+        />
+      </Box>
+    </>
   );
 };
 
