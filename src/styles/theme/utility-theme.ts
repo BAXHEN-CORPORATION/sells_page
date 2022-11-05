28d5459fca2d070507fc5ac140ea023b2d1b0ad1
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-11-05 15:32:41 -0300

    componetization

--- src/styles/theme/utility-theme.ts
+++ src/styles/theme/utility-theme.ts
@@ -1,7 +1,12 @@
 import { createTheme } from "@mui/material/styles";
 import breakpoints from "./base/breakpoints";
 import colors from "./base/colors";
+import functions from "./functions";
 
-const utilityTheme = createTheme({ breakpoints, palette: { ...colors } });
+const utilityTheme = createTheme({
+  breakpoints,
+  palette: { ...colors },
+  functions,
+});
 
 export default utilityTheme;
